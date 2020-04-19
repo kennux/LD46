e378cf938b23bb7b1c9fe412a5dd04737558d4ab
Daniele Marotta <danielemarotta.developer@gmail.com>
2020-04-19 20:10:30 +0200

    Implemented grid ui and more ui prefabs

---
+++ Assets/Code/UI/GameUIController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameUIController : MonoBehaviour
+{
+    public ReactorGridUIController ReactorGridUI;
+
+    private Game game;
+
+    private void Start()
+    {
+        game = new Game();
+        
+        ReactorGridUI.Initialize(game.reactor);
+    }
+
+    private void Update()
+    {
+        game.Update(Time.deltaTime);
+    }
+}
---
+++ Assets/Code/UI/ReactorGridUIController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ReactorGridUIController : MonoBehaviour
+{
+    public GridLayoutGroup gridLayout;
+
+    public ReactorGridCellUIController cellPrefab;
+
+    private Reactor reactor;
+
+    public void Initialize(Reactor reactor)
+    {
+        this.reactor = reactor;
+
+        SetupGridLayout();
+        CreateCells();
+    }
+
+    private void SetupGridLayout()
+    {
+        gridLayout.constraintCount = Game.ReactorSizeX;
+        
+        var gridRect = ((RectTransform) gridLayout.transform).rect;
+        var cellSize = Mathf.Min(gridRect.width / Game.ReactorSizeX, gridRect.height / Game.ReactorSizeY);
+        gridLayout.cellSize = new Vector2(cellSize, cellSize);
+    }
+
+    private void CreateCells()
+    {
+        for (var y = 0; y < Game.ReactorSizeY; y++)
+        {
+            for (var x = 0; x < Game.ReactorSizeX; x++)
+            {
+                Instantiate(cellPrefab, gridLayout.transform);
+            }
+        }
+    }
+}
